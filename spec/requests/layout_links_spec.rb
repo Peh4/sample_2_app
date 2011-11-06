0c89f990eb61247141127f99655b73ef94d2afd2
peh4 <peha@free.fr>
2011-11-06 17:42:32 +0100

    added layout links wuith routes

---
+++ spec/requests/layout_links_spec.rb
@@ -0,0 +1,27 @@
+require 'spec_helper'
+
+describe "LayoutLinks" do
+  
+  it "Should have a Home page at /" #do
+  #  get '/'
+  #  response.should have_selector('title', :content => 'home')
+  #end
+  #
+  #
+  #
+  #it "Should have Contact at /contact" do
+  #  get '/contact'
+  #  response.should have_selector('title', :content => 'Contact')
+  #end
+  #
+  #it "Should have about at /about" do
+  #  get '/about'
+  #  response.should have_selector('title', :content => 'About')
+  #end
+  #
+  #it "Should have Contact at /help" do
+  #  get '/help'
+  #  response.should have_selector('title', :content => 'help')
+  #end
+  
+end
