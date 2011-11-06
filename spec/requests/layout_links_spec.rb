cd80c43fe346416dd169b24673867d9a8690361f
peh4 <peha@free.fr>
2011-11-06 18:37:21 +0100

    added a user controller + layout links

--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -1,27 +1,44 @@
 require 'spec_helper'
 
 describe "LayoutLinks" do
+
+  it "should have a Home page at '/'" do
+    get '/'
+    response.should have_selector('title', :content => "Home")
+  end
+
+  it "should have a Contact page at '/contact'" do
+    get '/contact'
+    response.should have_selector('title', :content => "Contact")
+  end
+
+  it "should have an About page at '/about'" do
+    get '/about'
+    response.should have_selector('title', :content => "About")
+  end
+
+  it "should have a Help page at '/help'" do
+    get '/help'
+    response.should have_selector('title', :content => "Help")
+  end
   
-  it "Should have a Home page at /" #do
-  #  get '/'
-  #  response.should have_selector('title', :content => 'home')
-  #end
-  #
-  #
-  #
-  #it "Should have Contact at /contact" do
-  #  get '/contact'
-  #  response.should have_selector('title', :content => 'Contact')
-  #end
-  #
-  #it "Should have about at /about" do
-  #  get '/about'
-  #  response.should have_selector('title', :content => 'About')
-  #end
-  #
-  #it "Should have Contact at /help" do
-  #  get '/help'
-  #  response.should have_selector('title', :content => 'help')
-  #end
+  it "should have a Sign up page at '/sign up'" do
+    get '/signup'
+    response.should have_selector('title', :content => "Sign up")
+  end
+  
+  it "should have the right links on the layout" do
+    visit root_path
+    click_link "About"
+    response.should have_selector('title', :content => "About")
+    click_link "Help"
+    response.should have_selector('title', :content => "Help")
+    click_link "Contact"
+    response.should have_selector('title', :content => "Contact")
+    click_link "Home"
+    response.should have_selector('title', :content => "Home")
+    click_link "Sign in"
+    response.should have_selector('title', :content => "Sign up")
+  end
   
 end
